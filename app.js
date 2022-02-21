45665ceb690d04ced0c5032820ff98672a82ba08
Lior <levydlior@gmail.com>
2022-02-21 16:52:09 -0700

    changed curser on hover for pawn

--- app.js
+++ app.js
@@ -8,16 +8,23 @@ const moveLine = () =>{
 
 const moveUp = () =>{
 
-    let num = Math.floor(Math.random()* (1 - 557 +1))
+    let num = Math.floor(Math.random()* (1 - 558 +1))
     return num
 }
 
- pawn.addEventListener( 'click', function(){
+const moveFunc = () =>{
+
     const newNum= parseInt(moveLine())
-     pawn.style.left= `${newNum}rem` 
-     const newUp= parseInt(moveUp())
-     pawn.style.bottom= `${newUp}px` 
-   
+    pawn.style.left= `${newNum}rem` 
+    const newUp= parseInt(moveUp())
+    pawn.style.bottom= `${newUp}px` 
+}
+
+
+
+ pawn.addEventListener( 'click', function(){
+    moveFunc();
+
  })
 
 
